47a1e81ebf4371aac604e020c994f79ee52580be
EliezerQSilveira <168377587+EliezerQSilveira@users.noreply.github.com>
2025-04-16 18:03:53 -0300

    modificado-valor-de-entrada-e-saida

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+
+
+
+
+
 function generateNumber(){
     const min = Math.ceil (document.querySelector(".min").value)
     const max = Math.floor (document.querySelector(".max").value)
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
+    if (min <= max ){
 
     const resultDiv = document.getElementById("result");
     resultDiv.textContent = `Número sorteado: ${result}`;
@@ -11,7 +17,11 @@ function generateNumber(){
     resultDiv.classList.add("show");
 }
 
+else {
+
+ alert ("O numero de entrada deve ser menor que o de saida")
 
 
-
+    }
+}
 
